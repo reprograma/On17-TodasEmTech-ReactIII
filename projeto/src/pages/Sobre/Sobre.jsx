f4f38d8f2757a5797865b929529a27db43320133
Nana Miranda <falecomnanamiranda@gmail.com>
2022-07-21 12:54:08 -0300

    Entrega projeto

--- projeto/src/pages/Sobre/Sobre.jsx
+++ projeto/src/pages/Sobre/Sobre.jsx
@@ -1,7 +1,17 @@
 function Sobre() {
-  return(
-   <h1>Sobre</h1>
-  )
- }
- 
-export default Sobre
+  import Header from '../../components/Header/Header'
+  import './sobre.css'
+  import eu from '../../assets/Perfil.jpg'
+  
+  function Sobre({image, description}) {
+    return(
+     <h1>Sobre</h1>
+      <Header 
+        image={image} 
+        description="minha foto">
+        <img src={eu} alt={description}/>
+        <h2>Meu nome é Nana</h2>
+      </Header>
+    )
+   } 
+  }
